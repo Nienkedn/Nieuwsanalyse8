3444f44fe0f5f139312775a66b95db7c82003f23
jogamer275 <jordygo4it@gmail.com>
2019-05-14 14:24:03 +0200

    Changed the code to filter on important words

--- WindmillProject/modellen/K-means_test.py
+++ WindmillProject/modellen/K-means_test.py
@@ -1,26 +1,27 @@
-import matplotlib.pyplot as plt
-import seaborn as sns; sns.set()  # for plot styling
-import numpy as np
-
-# random generate blobs
-# n_samples are the quantity of the blobs, centers are the subdivide of centers,
-# cluster_std is the standard deviation of the clusters and random_state is the dataset generation
-from sklearn.datasets.samples_generator import make_blobs
-X, y_true = make_blobs(n_samples=300, centers=4,
-                       cluster_std=0.60, random_state=0)
-plt.scatter(X[:, 0], X[:, 1], s=50);
-
-# prediction of the clusters and divide the samples in clusters
+from sklearn.feature_extraction.text import TfidfVectorizer
+from stop_words import get_stop_words
 from sklearn.cluster import KMeans
-kmeans = KMeans(n_clusters=4)
-kmeans.fit(X)
-y_kmeans = kmeans.predict(X)
+from sklearn.metrics import adjusted_rand_score
 
-# Plotting random generated circles (samples)
-plt.scatter(X[:, 0], X[:, 1], c=y_kmeans, s=50, cmap='viridis')
+documents = open("../Data/omroep_zeeland.csv", mode="r", encoding="utf-16")
 
-# plotting the cluster centers
-centers = kmeans.cluster_centers_
-plt.scatter(centers[:, 0], centers[:, 1], c='white', s=300, alpha=1);
+stop_words = get_stop_words('dutch')
+vectorizer = TfidfVectorizer(stop_words)
+X = vectorizer.fit_transform(documents)
 
-plt.show();
+true_k = 2
+model = KMeans(n_clusters=true_k, init='k-means++', max_iter=100, n_init=1)
+model.fit(X)
+
+print("Top terms per cluster:")
+order_centroids = model.cluster_centers_.argsort()[:, ::-1]
+terms = vectorizer.get_feature_names()
+for i in range(true_k):
+    print("Cluster %d:" % i),
+    for ind in order_centroids[i, :10]:
+        print(' %s' % terms[ind]),
+    print
+
+print("\n")
+print("Prediction")
+

eccbff4b1a562d754563a4091a1df2ee9f07ee64
Gijs van den Abeele <gijsvdabeele@gmail.com>
2019-05-23 15:20:57 +0200

    Added lasso coefficient analysis

--- WindmillProject/models/lasso.py
+++ WindmillProject/models/lasso.py
@@ -5,8 +5,8 @@ import matplotlib.pyplot as plt
 import datetime as dt
 
 wordcount = []
-zoekwoord = 'wind'
-with open('../Data/tweets.csv', 'r', encoding='utf-16') as file:
+zoekwoord = 'windmolen'
+with open('../Data/data2.csv', 'r', encoding='utf-16') as file:
     data = csv.reader(file)
     next(data)
     for row in data:
@@ -18,12 +18,17 @@ with open('../Data/tweets.csv', 'r', encoding='utf-16') as file:
     fit_data = []
     for i, row in enumerate(data):
         temp_array = []
-        temp_array.append(int(row[1]))
+        temp_array.append(float(row[1]))
         temp_array.append(len(row[2]))
+        temp_array.append(int(row[4]))
+        temp_array.append(int(row[5]))
+        temp_array.append(float(row[6]))
+        temp_array.append(float(row[7]))
+
         fit_data.append(temp_array)
 
 # Decide on model and fit train data with targets
-reg = linear_model.Lasso(alpha=0.05)
+reg = linear_model.Lasso(alpha=0.0001)
 reg.fit(fit_data[:1000], wordcount[:1000])
 
 # Try to predict targets with new data
@@ -36,16 +41,18 @@ for i in fit_data:
 
 print(lasso)
 print("Avg:"+str(sum(lasso) / len(lasso)))
+print(f"Coefficients:\n{reg.coef_}")
+print(f"Coefficients:\n{reg.sparse_coef_}")
 
-plt.xlabel('Timestamps')
-plt.ylabel('Number of times the word shows up')
-plt.title('Popularity of the word '+zoekwoord)
-plt.grid(True)
-
-y = []
-for dot in wordcount[1001:2700]:
-    y.append(dot)
-plt.scatter(timestamps[1001:2700], y, s=2)
-
-plt.plot(timestamps[1001:2700], lasso, linewidth=0.5)
-plt.show()
+# plt.xlabel('Timestamps')
+# plt.ylabel('Number of times the word shows up')
+# plt.title('Popularity of the word '+zoekwoord)
+# plt.grid(True)
+#
+# y = []
+# for dot in wordcount[1001:2700]:
+#     y.append(dot)
+# plt.scatter(timestamps[1001:2700], y, s=2)
+#
+# plt.plot(timestamps[1001:2700], lasso, linewidth=0.5)
+# plt.show()

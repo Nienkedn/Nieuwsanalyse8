b78988af03397e72d3cb0a1b85320d6b12980457
stefandouw <stefan.douw@hotmail.com>
2019-05-23 10:15:55 +0200

    I add start of k-means sentiment

---
+++ WindmillProject/models/K-means_sentiment.py
@@ -0,0 +1,15 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+
+
+def get_sub_pol_array()
+
+
+def test():
+    print(test)
+
+
+if __name__ == "__main__":
+    """ This is executed when run from the command line """
+    test()

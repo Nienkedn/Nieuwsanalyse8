c26ae7b08fb44c31d4862691d23c3ea2fed90f1b
jogamer275 <jordygo4it@gmail.com>
2019-05-23 10:51:17 +0200

    fixed some issues with the stop_words and added a random state

--- WindmillProject/models/K-means_test.py
+++ WindmillProject/models/K-means_test.py
@@ -5,16 +5,16 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import adjusted_rand_score
 
 omroepzeeland = open("../Data/omroep_zeeland.csv", mode="r", encoding="utf-16")
-nu = open("../Data/data.csv", mode="r", encoding="utf-16")
+nu = open("../Data/nu_nl.csv", mode="r", encoding="utf-16")
 tweets = open("../Data/tweets.csv", mode="r", encoding="utf-16")
 
 stop_words = get_stop_words('dutch')
-vectorizer = TfidfVectorizer(stop_words)
+vectorizer = TfidfVectorizer(stop_words = stop_words)
 X = vectorizer.fit_transform(omroepzeeland)
 terms = vectorizer.get_feature_names()
 
 true_k = 4
-model = KMeans(n_clusters=true_k, init='k-means++', max_iter=100, n_init=1)
+model = KMeans(n_clusters=true_k, init='k-means++', max_iter=100, n_init=1000, random_state=5)
 model.fit(X)
 
 terms_vectorized = vectorizer.transform(terms)
@@ -24,10 +24,10 @@ print("Top terms per cluster:")
 order_centroids = model.cluster_centers_.argsort()[:, ::-1]
 # terms = vectorizer.get_feature_names()
 for i in range(true_k):
-    print("Cluster %d:" % i),
-    for ind in order_centroids[i, :10]:
-        print(' %s' % terms[ind]),
-    print
+   print("Cluster %d:" % i),
+   for ind in order_centroids[i, :10]:
+       print(' %s' % terms[ind]),
+   print
 
 print("\n")
 print("Prediction")
